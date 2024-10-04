609758604df024857f9c6ce90e99425ae3786303
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-10-04 19:21:12 +0700

    chore: add express-session, passport dependencies

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const dotenv = require("dotenv");
+require("dotenv").config();
 const express = require("express");
+const passport = require("passport");
+const session = require("express-session");
+const GoogleStrategy = require("passport-google-oauth20").Strategy;
 const cors = require("cors");
 
 // Initialize application and server
@@ -15,6 +19,37 @@ app.use(
 );
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: "secret",
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: "http://localhost:4000/api/auth/google/callback",
+    },
+    (accessToken, refreshToken, profile, done) => {
+      return done(null, profile);
+    }
+  )
+);
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+app.get("/", (req, res) => {
+  res.send("<a href='/api/auth/google'>Login with Google</a>");
+});
 
 app.use("/api", require("./routes/AuthRoute"));
 
