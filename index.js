a832c1e460b0401be4f09b13a839f19f53b9eab3
QuangNV23062004 <nguyenquang23062004@gmail.com>
2024-10-10 10:19:36 +0700

    remove session and passport

--- index.js
+++ index.js
@@ -33,54 +33,6 @@ const vimeoClient = new Vimeo(
   process.env.VIMEO_ACCESS_TOKEN
 );
 
-app.use(
-  session({
-    secret: "secret",
-    resave: false,
-    saveUninitialized: true,
-  })
-);
-app.use(passport.initialize());
-app.use(passport.session());
-
-// Google OAuth
-passport.use(
-  new GoogleStrategy(
-    {
-      clientID: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "http://localhost:4000/api/auth/google/callback",
-    },
-    (accessToken, refreshToken, profile, done) => {
-      return done(null, profile);
-    }
-  )
-);
-
-passport.use(
-  new AppleStrategy(
-    {
-      clientID: process.env.APPLE_CLIENT_ID,
-      teamID: process.env.APPLE_TEAM_ID,
-      callbackURL:
-        "https://social-media-ofm3.onrender.com/api/auth/apple/callback",
-      keyID: process.env.APPLE_KEY_ID,
-      // privateKeyLocation: `./config/AuthKey_${process.env.APPLE_KEY_ID}.p8`,
-      privateKeyString: process.env.APPLE_PRIVATE_KEY,
-      passReqToCallback: true,
-    },
-    (req, accessToken, refreshToken, idToken, profile, done) => {
-      return done(null, profile);
-    }
-  )
-);
-
-passport.serializeUser((user, done) => {
-  done(null, user);
-});
-passport.deserializeUser((user, done) => {
-  done(null, user);
-});
 app.get("/", (req, res) => {
   res.send(
     "<a href='/api/auth/google'>Login with Google</a><br>" +
