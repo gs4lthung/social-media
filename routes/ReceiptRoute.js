0ea88423d7b7f5aea849dc49367d5a53c5e78028
Lâm Tiên Hưng <89993051+gs4lthung@users.noreply.github.com>
2024-10-16 05:01:42 +0700

    Update ReceiptRoute.js

--- routes/ReceiptRoute.js
+++ routes/ReceiptRoute.js
@@ -8,8 +8,70 @@ const route = express.Router();
 // Apply the authentication mid dleware to all routes
 route.use(AuthMiddleware);
 
+/**
+ * @swagger
+ * /api/receipts:
+ *   get:
+ *     security:
+ *      - bearerAuth: []
+ *     summary: Get all user receipts
+ *     tags: [Receipts]
+ *     responses:
+ *       200:
+ *         description: Get all user receipts successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 route.get("/", receiptController.getAllUserReceiptsController);
+
+/**
+ * @swagger
+ * /api/receipts/{id}:
+ *   get:
+ *     security:
+ *      - bearerAuth: []
+ *     summary: Get a receipt by ID
+ *     tags: [Receipts]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *         type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Get a receipt successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 route.get("/:id", receiptController.getReceiptController);
+
+/**
+ * @swagger
+ * /api/receipts/{id}:
+ *   delete:
+ *     security:
+ *      - bearerAuth: []
+ *     summary: Delete a receipt by ID
+ *     tags: [Receipts]
+ *     parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *         type: string
+ *         required: true
+ *     responses:
+ *       200:
+ *         description: Delete a receipt successfully
+ *       400:
+ *         description: Bad request
+ *       500:
+ *         description: Internal server error
+ */
 route.delete("/:id", receiptController.deleteReceiptController);
 
 module.exports = route;
